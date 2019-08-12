a9e3a4a6bf9231a4c1786fdea9d597a61720e50d
유키유키 <pib1t@naver.com>
2019-08-12 17:31:41 +0900

    prototype

--- src/App.tsx
+++ src/App.tsx
@@ -2,62 +2,12 @@ import React, { useState, useEffect } from 'react'
 import './App.css'
 import { Button } from './components/Button'
 
-const useOperator = () => {
-
-}
-
-const useNumber = () => {
-
-  const [number, setNumber] = useState(0)
-
-  const plusNumber = (value: number) => setNumber(number + value)
-  const minusNumber = (input: number) => setNumber(number - input)
-
-  useEffect(() => {
-    document.title = String(number)
-  })
-  return [plusNumber, minusNumber]
-}
 
 const Calc = () => {
-  const [plusNumber, minusNumber] = useNumber()
+  // const [counter, setCounter] = useCounter()
+  const [counter, setCounter] = useState<string>('')
   return (
-    <table>
-      <thead>
-        <th colSpan={4}>0</th>
-      </thead>
-      <tbody>
-      <tr>
-        <Button className="first-button">AC</Button>
-        <Button className="first-button">+/-</Button>
-        <Button className="first-button">%</Button>
-        <Button>÷</Button>
-      </tr>
-      <tr>
-        <Button>7</Button>
-        <Button>8</Button>
-        <Button>9</Button>
-        <Button>X</Button>
-      </tr>
-      <tr>
-        <Button>4</Button>
-        <Button>5</Button>
-        <Button>6</Button>
-        <Button handlerClick={() => minusNumber(23)}>-</Button>
-      </tr>
-      <tr>
-        <Button>1</Button>
-        <Button>2</Button>
-        <Button>3</Button>
-        <Button>+</Button>
-      </tr>
-      <tr>
-        <Button colSpan={2}>0</Button>
-        <Button>.</Button>
-        <Button>=</Button>
-      </tr>
-      </tbody>
-    </table>
+    <Button onPress={ setCounter } counter={ counter }/>
   )
 }
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,60 @@
 import React from 'react'
 
+
 export const Button = (props: any) => {
+
+  const { onPress, counter } = props
+  console.log(onPress)
+  console.log("LOADED")
   return (
-    <td colSpan={props.colSpan}>
-      {props.children}
-    </td>
+    <div>
+      <table> 
+        <thead>
+          <tr>
+            <th colSpan={4}> {counter} </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="first-td">AC</td>
+            <td className="first-td">+/-</td>
+            <td className="first-td">%</td>
+            <td>÷</td>
+          </tr>
+          <tr>
+            <NumberButton countValue={3} onPress={() => {}}/>
+            <td onClick={ () => {console.log("CLICKED")} }>8</td>
+            <td>9</td>
+            <td>X</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>+</td>
+          </tr>
+          <tr>
+            <td colSpan={2}>0</td>
+            <td>.</td>
+            <td>=</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+
+const NumberButton = (props: any) => {
+  const { countValue, onPress } = props
+
+  return (
+    <td onClick={ () => { onPress(countValue) }}>{countValue}</td>
   )
 }
