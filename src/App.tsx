c30dab89739b822238e73403b2741b0438191526
Yukyuki <pib1t@naver.com>
2019-08-13 14:39:06 +0900

    Final xD

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { Button } from './components/Button'
 
 
 const Calc = () => {
-  // const [counter, setCounter] = useCounter()
   const [counter, setCounter] = useState<string>('')
   return (
     <Button onPress={ setCounter } counter={ counter }/>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
-
+import { evaluate } from 'mathjs'
 
 export const Button = (props: any) => {
 
   const { onPress, counter } = props
-  console.log(onPress)
-  console.log("LOADED")
+
+  const addToCounter = (value: string) => {
+    // dot point
+    if ( value === '.' && (counter.length === 0
+      || counter.slice(-1) === '.')) return
+
+    if (counter === 'ERROR') onPress('')
+    else onPress(counter + value)
+  }
+
+  const operator = (value: string) => {
+    switch (value) {
+      case 'AC': onPress(''); break
+      case '=':
+        try {
+          onPress( evaluate(counter) )
+        } catch (e) {
+         console.error("Eval Error", e)
+         onPress('ERROR')
+        }
+        break
+      case '+/-':
+        console.log(counter)
+        const prefix = String(counter).charAt(0)
+        if (prefix === "" || prefix !== "-") onPress('-' + counter)
+        else onPress(String(counter).slice(1))
+        break
+    }
+
+  }
   return (
     <div>
       <table> 
@@ -16,45 +44,36 @@ export const Button = (props: any) => {
         </thead>
         <tbody>
           <tr>
-            <td className="first-td">AC</td>
-            <td className="first-td">+/-</td>
-            <td className="first-td">%</td>
-            <td>÷</td>
+            <td className="first-td" onClick={ () => {operator('AC')}}>AC</td>
+            <td onClick={ () => {operator('+/-')} } className="first-td">+/-</td>
+            <td onClick={ () => {addToCounter('%')} } className="first-td">%</td>
+            <td onClick={ () => {addToCounter('/')} }>÷</td>
           </tr>
           <tr>
-            <NumberButton countValue={3} onPress={() => {}}/>
-            <td onClick={ () => {console.log("CLICKED")} }>8</td>
-            <td>9</td>
-            <td>X</td>
+            <td onClick={ () => {addToCounter('7')} }>7</td>
+            <td onClick={ () => {addToCounter('8')} }>8</td>
+            <td onClick={ () => {addToCounter('9')} }>9</td>
+            <td onClick={ () => {addToCounter('*')} }>X</td>
           </tr>
           <tr>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>-</td>
+            <td onClick={ () => {addToCounter('4')} }>4</td>
+            <td onClick={ () => {addToCounter('5')} }>5</td>
+            <td onClick={ () => {addToCounter('6')} }>6</td>
+            <td onClick={ () => {addToCounter('-')} }>-</td>
           </tr>
           <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>+</td>
+            <td onClick={ () => {addToCounter('1')} }>1</td>
+            <td onClick={ () => {addToCounter('2')} }>2</td>
+            <td onClick={ () => {addToCounter('3')} }>3</td>
+            <td onClick={ () => {addToCounter('+')} }>+</td>
           </tr>
           <tr>
-            <td colSpan={2}>0</td>
-            <td>.</td>
-            <td>=</td>
+            <td onClick={ () => {addToCounter('0')} } colSpan={2}>0</td>
+            <td onClick={ () => {addToCounter('.')} }>.</td>
+            <td onClick={ () => {operator('=')} }>=</td>
           </tr>
         </tbody>
       </table>
     </div>
-  )
-}
-
-
-const NumberButton = (props: any) => {
-  const { countValue, onPress } = props
-
-  return (
-    <td onClick={ () => { onPress(countValue) }}>{countValue}</td>
   )
 }
